af4d0e32d6dea98fba692656a40618fe9f937cca
seif <baazouziseif9@gmail.com>
2022-09-04 10:54:58 +0100

    clean server code

--- server/server.go
+++ server/server.go
@@ -15,18 +15,6 @@ type Message struct {
 	Buffer []byte
 }
 
-func deserialize(buffer []byte) (Message, error) {
-	var message Message
-
-	b := bytes.Buffer{}
-	b.Write(buffer)
-
-	decoder := gob.NewDecoder(&b)
-	err := decoder.Decode(&message)
-
-	return message, err
-}
-
 var (
 	connections = make(map[string][]*websocket.Conn)
 )
@@ -40,20 +28,23 @@ func SocketHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer conn.Close()
+	handlerEventLoop(conn)
+}
 
+func handlerEventLoop(conn *websocket.Conn) {
 	for {
 		messageType, buffer, err := conn.ReadMessage()
 
 		if err != nil {
 			log.Println("Error during message reading: ", err)
-			break
+			return
 		}
 
 		message, err := deserialize(buffer)
 
 		if err != nil {
 			log.Println("Error during deserialize message: ", err)
-			break
+			return
 		}
 
 		if message.Event == "subscribe" {
@@ -72,13 +63,29 @@ func SocketHandler(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
-		for _, conn := range connections[message.Event] {
-			err = conn.WriteMessage(messageType, buffer)
+		sendMessagesToSubscribers(message.Event, messageType, buffer)
+	}
+}
 
-			if err != nil {
-				log.Println("Error during message writing:", err)
-				continue
-			}
+func sendMessagesToSubscribers(event string, messageType int, buffer []byte) {
+	for _, conn := range connections[event] {
+		err := conn.WriteMessage(messageType, buffer)
+
+		if err != nil {
+			log.Println("Error during message writing:", err)
+			continue
 		}
 	}
+}
+
+func deserialize(buffer []byte) (Message, error) {
+	var message Message
+
+	b := bytes.Buffer{}
+	b.Write(buffer)
+
+	decoder := gob.NewDecoder(&b)
+	err := decoder.Decode(&message)
+
+	return message, err
 }
